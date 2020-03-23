d770cc68a212fdbb478c4f9be64a852833f2c415
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2020-03-23 23:36:14 +0000

    still playing

--- doubling_data.R
+++ doubling_data.R
@@ -5,13 +5,17 @@ source("seq_double.R")
 
 every_1_day <- tibble(cases = seq_double(2,60), date_1 = ymd(ymd("2020-03-03") + 
                         seq(1, length.out = length(cases), by = 1))) %>%
-  filter(date_1 <= today())
+  filter(date_1 <= today()) %>%
+  mutate(days_since = as.numeric(date_1 - min(date_1)))
 every_2_day <- tibble(cases = seq_double(2,60), date_2 = ymd("2020-03-03") + 
                         seq(1, length.out = length(cases), by = 2)) %>%
-  filter(date_2 <= today())
+  filter(date_2 <= today()) %>%
+  mutate(days_since = as.numeric(date_2 - min(date_2)))
 every_3_day <- tibble(cases = seq_double(2,60), date_3 = ymd("2020-03-03") + 
                         seq(1, length.out = length(cases), by = 3)) %>%
-  filter(date_3 <= today())
+  filter(date_3 <= today()) %>%
+  mutate(days_since = as.numeric(date_3 - min(date_3)))
 every_4_day <- tibble(cases = seq_double(2,60), date_4 = ymd("2020-03-03") + 
                         seq(1, length.out = length(cases), by = 4)) %>%
-  filter(date_4 <= today())
+  filter(date_4 <= today()) %>%
+  mutate(days_since = as.numeric(date_4 - min(date_4)))
--- plot_doubling.R
+++ plot_doubling.R
@@ -6,11 +6,17 @@ source("doubling_data.R")
 
 covid_states <- read_csv("http://covidtracking.com/api/states/daily.csv") %>%
   select(date, state:total) %>%
-  mutate(date = ymd(date))
+  mutate(date = ymd(date)) %>%
+  ungroup() %>%
+  filter(total > 0) %>%
+  arrange(date) %>% 
+  group_by(state) %>%
+  mutate(days_since = as.numeric(date - min(date))) %>%
+  ungroup()
 
 covid_states_gg <- covid_states %>%
-  filter(state == "RI" | state == "NY") %>%
-  ggplot(aes(x = date, y = positive, color = state)) +
+  filter(state %in% c("RI", "NY", "MA", "WA", "CA")) %>%
+  ggplot(aes(x = days_since, y = positive, color = state)) +
   geom_line() +
   geom_line(data = every_1_day, aes(x=date_1, y = cases), color = "black", size = 1.5) +
   geom_line(data = every_2_day, aes(x=date_2, y = cases), color = "red", size = 1.5) +
